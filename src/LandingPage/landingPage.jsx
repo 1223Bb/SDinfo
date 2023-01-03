6e38b1e768e8734f335cbe961ddb28dbe7794c74
1223Bb <brunoscachetti@hotmail.com>
2023-01-03 13:09:14 -0300

    Purge + New website

--- src/LandingPage/landingPage.jsx
+++
@@ -1,9 +0,0 @@
-﻿function LandingPage(){
-	return <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">SD Informática</a>
-  </div>
-</nav>
-}
-
-export default LandingPage;
